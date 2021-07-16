009652935b7f717f5a1c6b52e09f3bd8dc50775a
Lalo Sanchez <laloptk@gmail.com>
2021-07-15 20:49:39 -0500

    Remove uneccesary code and style the code better

--- inc/options-api/classes/classesSettings.php
+++ inc/options-api/classes/classesSettings.php
@@ -32,17 +32,23 @@ class SettingsPage implements Settings {
 	public $args;
 
 	public function __construct(Array $args) {
+		
 		$this->args = $args;
+		
 		$this->page_slug = $args['page_slug'];
 
 		add_action('admin_menu', array($this, 'render_settings_page' ));
+
 		add_action('admin_init', array($this, 'render_settings_sections' ));
+
 		add_action('admin_init', array($this, 'render_settings_fields' ));
+
 	}
 
 	public function get_args(object $sender) {
 		
 		if($sender instanceof AddSettingsPage) {
+			
 			$this->page = array(
 				'page_title' => $this->args['page_title'], 
 				'menu_title' => $this->args['menu_title'],
@@ -50,38 +56,49 @@ class SettingsPage implements Settings {
 			);
 
 			$sender->add_page();
+
 		}
 
 		if($sender instanceof AddSettingsSections) {
-			$this->sections = $this->args['sections'];
-			foreach($this->sections as $section_id => $section_args) {
+			
+			foreach($this->args['sections'] as $section_id => $section_args) {
 				$sender->add_section($section_id, $section_args['title']);
-			}			
+			}
+
 		}
 
 		if($sender instanceof AddSettingsFields) {
-			$this->fields = array();
 			foreach($this->args['sections'] as $section_id => $section_args ) {
+				
 				foreach( $section_args['fields'] as $field_id => $field ) {
 					$field['section_id'] = $section_id;
-					$this->fields[$field_id][] = $field;
 					$sender->add_field( $field_id, $field );
 				}
-			}	
+
+			}
+
 		}
+
 	}
 
 	public function render_settings_page() {
+
 		new AddSettingsPage($this, array());
+
 	}
 
 	public function render_settings_sections() {
-		new AddSettingsSections($this, array());		
+
+		new AddSettingsSections($this, array());
+
 	}
 
 	public function render_settings_fields() {
+
 		new AddSettingsFields($this, array());
+
 	}
+
 }
 
 /**
@@ -92,25 +109,24 @@ class SettingsPage implements Settings {
  */
 
 class ConnectWithSettingsPage {
+	
 	public $settings;
+
 	public $args;
 	
-	public function __construct(SettingsPage $settings = null, Array $args = array()) {
-		
+	public function __construct(SettingsPage $settings = null, Array $args = array()) {		
 		
 		$this->settings = $settings;		
 		$this->settings->get_args($this);
-		//var_dump($this->settings->page_slug);
-		//die();
-
 
 	}
 
-	//abstract public function callback();
 }
 
 class AddSettingsPage extends ConnectWithSettingsPage {
-	public function add_page() {	
+	
+	public function add_page() {
+
 		add_menu_page(
 			$this->settings->page['page_title'], 
 			$this->settings->page['menu_title'],
@@ -133,18 +149,21 @@ class AddSettingsPage extends ConnectWithSettingsPage {
 		echo	'</form>
 			</div>';
 
-	}	
+	}
+
 }
 
 class AddSettingsSections extends ConnectWithSettingsPage {
 
 	public function add_section($section_id, $section_title) {
+		
 		add_settings_section( 
 			$section_id,
 			$section_title,
 			array($this, 'callback'),
 			$this->settings->page_slug				
 		);
+
 	}	
 
 	public function callback() {
@@ -173,18 +192,23 @@ class AddSettingsFields extends ConnectWithSettingsPage {
 				$field_args['section_id'],
 				$callback_args
 			);
+
 	}
 
 	public function callback($args) {
 
 		switch($args['type']) {
+
 			case 'text':
 				echo '<input type="text" name="' . esc_attr($args['field_id']) . '" id="' . esc_attr($args['fie ld_id']) . '" value="' . esc_attr($args['value'] ? $args['value'] : '') . '" />';
 				break;
+
 			case 'textarea': {
 				echo '<textarea name="' . esc_attr($args['field_id']) . '" id="' . esc_attr($args['field_id']) . '" >' . esc_html($args['value'] ? $args['value'] : '') . '</textarea>';
 				break;
+
 			}
+
 		}
 
 	}
@@ -195,56 +219,3 @@ class AddSettingsFields extends ConnectWithSettingsPage {
 
 	}
 }
-
-/*******************************************************************
-* 	This is how I have been testing while coding,
-*   This will change to make it less redundant and convoluted
-*******************************************************************/
-
-		
-
-/*add_action('admin_init', 'add_everything');
-function add_everything() {
-	$args = array(
-		'page_title' => "This is a cool title", 
-		'menu_title' => "Test",
-		'capability' => "manage_options", 
-		'page_slug'  => "lalo_settings",
-		'sections'   => array(
-			'eg_section_1' => array(
-				'title' => 'This is the first example title',
-				'fields' => array(
-					'input_text_2' => array(
-						'title' => 'First field title 2',
-						'label' => 'label 1',
-						'class' => 'class',
-						'type' => 'text'
-					)
-				)
-			),
-			'eg_section_2' => array(
-				'title' => 'This is the second example title',
-				'fields' => array(
-					'input_text_1' => array( 
-						'title' => 'First field title',
-						'label' => 'label 2',
-						'class' => 'class',
-						'type' => 'text'
-					),
-					'textarea_id' => array(
-						'title' => 'First textarea',
-						'label' => 'label 2',
-						'class' => 'class',
-						'type' => 'textarea'
-					)
-				)
-			)
-		)
-	);
-
-
-	$renderPage = new SettingsPage($args);
-	$addSettingsSections = new AddSettingsSections($renderPage, array());
-	$addSettingsFields = new AddSettingsFields($renderPage, array());
-}*/
-
--- reusable-classes.php
+++ reusable-classes.php
@@ -14,65 +14,6 @@
 
 include( __DIR__ . '/inc/options-api/classes/classesSettings.php');
 
-/*$args = array(
-    'page_title' => "This is a cool title", 
-    'menu_title' => "Test",
-    'capability' => "manage_options", 
-    'page_slug'  => "lalos_options",
-    'sections'   => array(
-        'eg_section_1' => array(
-            'title' => 'This is the first example title',
-            'fields' => array(
-                'input_text_2' => array(
-                    'title' => 'First field title 2',
-                    'label' => 'label 1',
-                    'class' => 'class',
-                    'type' => 'text'
-                )
-            )
-        ),
-        'eg_section_2' => array(
-            'title' => 'This is the second example title',
-            'fields' => array(
-                'input_text_1' => array( 
-                    'title' => 'First field title',
-                    'label' => 'label 2',
-                    'class' => 'class',
-                    'type' => 'text'
-                ),
-                'textarea_id' => array(
-                    'title' => 'First textarea',
-                    'label' => 'label 2',
-                    'class' => 'class',
-                    'type' => 'textarea'
-                )
-            )
-        )
-    )
-);
-
-function add_settings_page($args) { 
-    $settingsPage = new SettingsPage($args); 
-    add_action('admin_menu', function() use ($args, $settingsPage) {
-        new AddSettingsPage($settingsPage, array());
-    });
-}
-
-function add_settings_page_sections_and_fields($args) {        
-    $settingsPage = new SettingsPage($args);
-    add_action('admin_init', function() use ($args, $settingsPage) {              
-        new AddSettingsSections($settingsPage, array());
-        new AddSettingsFields($settingsPage, array());
-    });
-
-}
-
-add_action('add_settings_page', 'add_settings_page', 2, 1);
-add_action('add_settings_page', 'add_settings_page_sections_and_fields', 10, 1);
-
-do_action('add_settings_page');
-*/
-
 $args = array(
     'page_title' => "This is a cool title", 
     'menu_title' => "Test",
@@ -110,21 +51,10 @@ $args = array(
     )
 );
 
-$settingsPage = new SettingsPage($args);
+// This is a wrapper for the concrete class
 
-//function initialize_settings_page($args) {
-    
-    /*add_action('admin_menu', function() use ($args) {    
-        $settingsPage = new SettingsPage($args);    
-        new AddSettingsPage($settingsPage, array());
-    });
+function add_settings_page($args) {
+    new SettingsPage($args);
+}
 
-    add_action('admin_init', function() use ($args) { 
-        $settingsPage = new SettingsPage($args);       
-        new AddSettingsSections($settingsPage, array());
-        new AddSettingsFields($settingsPage, array());
-    });*/
-//}
-
-//initialize_settings_page($args);
-
+add_settings_page($args);
